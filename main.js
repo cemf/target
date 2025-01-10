1a31269e4910c16e92a082ca15140ddfc6cf5f2e
Carlos <kaikelmfabricio@gmail.com>
2025-01-10 18:25:01 -0300

    feat: question 3

--- main.js
+++ main.js
@@ -13,4 +13,45 @@ function questao1(){
     
     return soma;
 }
+
+async function fetchData() {
+    const res = await fetch('./dados.json');
+    const data = await res.json();
+    handleDataQuestao3(data);
+  }
+
+
+const handleDataQuestao3 = (dados) => {
+    const lowestProfit = document.getElementById('lowestProfit');
+    const highestProfit = document.getElementById('highestProfit');
+    const daysAverage = document.getElementById('daysAverage');
+
+    const average = () =>{
+        let soma = 0;
+        let dias = 0;
+        for (let i = 0; i < dados.length; i++) {
+            if(dados[i].valor != 0){
+                soma += dados[i].valor;
+                dias++;
+            }
+            
+        }
+        return soma / dias;
+    }
+    console.log(average)
+    const values = dados => dados.map(dado => dado.valor);
+    let highest = Math.max(...values);
+    let positiveNumbers = values.filter((value) => value > 0);
+    let lowest = Math.min(...positiveNumbers);
+
+    lowestProfit.innerText = `Menor lucro: ${lowest}`;
+    highestProfit.innerText = `Maior lucro: ${highest}`;
+
+    let daysBestThanAverage = dados.filter(dado => dado.valor > average).length;
+
+    daysAverage.innerText = `Dias com lucro acima da média: ${daysBestThanAverage}`;
+}
+
 questao1();
+handleDataQuestao3();
+
--- question2.js
+++ question2.js
@@ -1,10 +1,14 @@
-function isInFibonacciSequence(n) {
-  let before = 0;
-  let after = 1;
-  while (after < n) {
-    let temp = before;
-    before = after;
-    after = temp + after;
+function isInFibonacciSequence(number) {
+  if (number < 0 || isNaN(number)) {
+    return "Invalid number";
+  } else {
+    let before = 0;
+    let after = 1;
+    while (after < n) {
+      let temp = before;
+      before = after;
+      after = temp + after;
+    }
+    return after === number;
   }
-  return after === n;
 }
